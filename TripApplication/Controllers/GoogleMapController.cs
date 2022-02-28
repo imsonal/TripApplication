894a4cf4f8a0ea4f6bc47a87c57407e73ee88de9
nikunj-arche <nikunj.pandya@archesoftronix.com>
2022-02-28 11:52:52 +0530

    applied trip distance calculations using google map API

---
+++ TripApplication/Controllers/GoogleMapController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TripApplication.Controllers
+{
+    public class GoogleMapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DistanceCalculation()
+        {
+            return View();
+        }
+    }
+}
